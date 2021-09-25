4de194c981fd0904053ef3b7e254bc426262b807
Jenae Janzen <63259035+Jjanzen27@users.noreply.github.com>
2021-09-25 16:37:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const Header = ({ course }) => (
+  <>
+    <h1>{course}</h1>
+  </>
+);
+
+const Content = ({ parts }) => {
+    console.log(parts)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
     </div>
-  );
+  )
+}
+
+const Part = ({ part }) => {
+  return <p>{part.name} {part.exercises}</p>
+}
+
+const Total = ({ parts }) => {
+  console.log(parts)
+  return <p>Total: {parts.reduce((a, c) => a + c.exercises, 0)}</p>
+}
+
+const App = () => {
+  const course = {
+    name: 'Ladders 101',
+    parts: [
+      {
+        name: 'First steps',
+        exercises: 10,
+      },
+      {
+        name: 'Rails',
+        exercises: 7,
+      },
+      {
+        name: 'Stairmastery',
+        exercises: 14,
+      },
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
 }
 
 export default App;
